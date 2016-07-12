072697bf0c058635a995a3936b39e5a6e7dea901
Andy Shapiro <shapiromatron@gmail.com>
2016-07-12 17:19:12 -0400

    used ordered dict for predictable model order

--- bmds/bmds.py
+++ bmds/bmds.py
@@ -1,5 +1,7 @@
 from . import models, constants
 
+from collections import OrderedDict
+
 
 class Session(object):
 
@@ -18,28 +20,28 @@ class Session(object):
 class BMDS_v230(Session):
 
     models = {
-        constants.DICHOTOMOUS: {
-            constants.M_Weibull: models.Weibull_215,
-            constants.M_LogProbit: models.LogProbit_32,
-            constants.M_Probit: models.Probit_32,
-            constants.M_Multistage: models.Multistage_32,
-            constants.M_Gamma: models.Gamma_215,
-            constants.M_Logistic: models.Logistic_213,
-            constants.M_LogLogistic: models.LogLogistic_213
-        },
-        constants.DICHOTOMOUS_CANCER: {
-            constants.M_MultistageCancer: models.MultistageCancer_19
-        },
-        constants.CONTINUOUS: {
-            constants.M_Linear: models.Linear_216,
-            constants.M_Polynomial: models.Polynomial_216,
-            constants.M_Power: models.Power_216,
-            constants.M_ExponentialM2: models.Exponential_M2_17,
-            constants.M_ExponentialM3: models.Exponential_M3_17,
-            constants.M_ExponentialM4: models.Exponential_M4_17,
-            constants.M_ExponentialM5: models.Exponential_M5_17,
-            constants.M_Hill: models.Hill_216,
-        },
+        constants.DICHOTOMOUS: OrderedDict([
+            (constants.M_Logistic, models.Logistic_213),
+            (constants.M_LogLogistic, models.LogLogistic_213),
+            (constants.M_Probit, models.Probit_32),
+            (constants.M_LogProbit, models.LogProbit_32),
+            (constants.M_Multistage, models.Multistage_32),
+            (constants.M_Gamma, models.Gamma_215),
+            (constants.M_Weibull, models.Weibull_215),
+        ]),
+        constants.DICHOTOMOUS_CANCER: OrderedDict([
+            (constants.M_MultistageCancer, models.MultistageCancer_19),
+        ]),
+        constants.CONTINUOUS: OrderedDict([
+            (constants.M_Linear, models.Linear_216),
+            (constants.M_Polynomial, models.Polynomial_216),
+            (constants.M_Power, models.Power_216),
+            (constants.M_Hill, models.Hill_216),
+            (constants.M_ExponentialM2, models.Exponential_M2_17),
+            (constants.M_ExponentialM3, models.Exponential_M3_17),
+            (constants.M_ExponentialM4, models.Exponential_M4_17),
+            (constants.M_ExponentialM5, models.Exponential_M5_17),
+        ]),
     }
 
 
@@ -50,56 +52,57 @@ class BMDS_v231(BMDS_v230):
 class BMDS_v240(BMDS_v231):
 
     models = {
-        constants.DICHOTOMOUS: {
-            constants.M_Weibull: models.Weibull_216,
-            constants.M_LogProbit: models.LogProbit_33,
-            constants.M_Probit: models.Probit_33,
-            constants.M_Multistage: models.Multistage_33,
-            constants.M_Gamma: models.Gamma_216,
-            constants.M_Logistic: models.Logistic_214,
-            constants.M_LogLogistic: models.LogLogistic_214,
-        },
-        constants.DICHOTOMOUS_CANCER: {
-            constants.M_MultistageCancer: models.MultistageCancer_110,
-        },
-        constants.CONTINUOUS: {
-            constants.M_Linear: models.Linear_217,
-            constants.M_Polynomial: models.Polynomial_217,
-            constants.M_Power: models.Power_217,
-            constants.M_ExponentialM2: models.Exponential_M2_19,
-            constants.M_ExponentialM3: models.Exponential_M3_19,
-            constants.M_ExponentialM4: models.Exponential_M4_19,
-            constants.M_ExponentialM5: models.Exponential_M5_19,
-            constants.M_Hill: models.Hill_217,
-        },
+        constants.DICHOTOMOUS: OrderedDict([
+            (constants.M_Logistic, models.Logistic_214),
+            (constants.M_LogLogistic, models.LogLogistic_214),
+            (constants.M_Probit, models.Probit_33),
+            (constants.M_LogProbit, models.LogProbit_33),
+            (constants.M_Multistage, models.Multistage_33),
+            (constants.M_Gamma, models.Gamma_216),
+            (constants.M_Weibull, models.Weibull_216),
+        ]),
+        constants.DICHOTOMOUS_CANCER: OrderedDict([
+            (constants.M_MultistageCancer,  models.MultistageCancer_110),
+        ]),
+        constants.CONTINUOUS: OrderedDict([
+            (constants.M_Linear, models.Linear_217),
+            (constants.M_Polynomial, models.Polynomial_217),
+            (constants.M_Power, models.Power_217),
+            (constants.M_Hill, models.Hill_217),
+            (constants.M_ExponentialM2, models.Exponential_M2_19),
+            (constants.M_ExponentialM3, models.Exponential_M3_19),
+            (constants.M_ExponentialM4, models.Exponential_M4_19),
+            (constants.M_ExponentialM5, models.Exponential_M5_19),
+        ]),
     }
 
 
 class BMDS_v260(BMDS_v240):
     models = {
-        constants.DICHOTOMOUS: {
-            constants.M_Weibull: models.Weibull_216,
-            constants.M_LogProbit: models.LogProbit_33,
-            constants.M_Probit: models.Probit_33,
-            constants.M_Multistage: models.Multistage_34,
-            constants.M_Gamma: models.Gamma_216,
-            constants.M_Logistic: models.Logistic_214,
-            constants.M_LogLogistic: models.LogLogistic_214,
-        },
-        constants.DICHOTOMOUS_CANCER: {
-            constants.M_MultistageCancer: models.MultistageCancer_110,
-        },
-        constants.CONTINUOUS: {
-            constants.M_Linear: models.Linear_220,
-            constants.M_Polynomial: models.Polynomial_220,
-            constants.M_Power: models.Power_218,
-            constants.M_ExponentialM2: models.Exponential_M2_110,
-            constants.M_ExponentialM3: models.Exponential_M3_110,
-            constants.M_ExponentialM4: models.Exponential_M4_110,
-            constants.M_ExponentialM5: models.Exponential_M5_110,
-            constants.M_Hill: models.Hill_217,
-        },
+        constants.DICHOTOMOUS: OrderedDict([
+            (constants.M_Logistic, models.Logistic_214),
+            (constants.M_LogLogistic, models.LogLogistic_214),
+            (constants.M_Probit, models.Probit_33),
+            (constants.M_LogProbit, models.LogProbit_33),
+            (constants.M_Multistage, models.Multistage_34),
+            (constants.M_Gamma, models.Gamma_216),
+            (constants.M_Weibull, models.Weibull_216),
+        ]),
+        constants.DICHOTOMOUS_CANCER: OrderedDict([
+            (constants.M_MultistageCancer, models.MultistageCancer_110),
+        ]),
+        constants.CONTINUOUS: OrderedDict([
+            (constants.M_Linear, models.Linear_220),
+            (constants.M_Polynomial, models.Polynomial_220),
+            (constants.M_Power, models.Power_218),
+            (constants.M_Hill, models.Hill_217),
+            (constants.M_ExponentialM2, models.Exponential_M2_110),
+            (constants.M_ExponentialM3, models.Exponential_M3_110),
+            (constants.M_ExponentialM4, models.Exponential_M4_110),
+            (constants.M_ExponentialM5, models.Exponential_M5_110),
+        ]),
     }
+
 
 class BMDS_v2601(BMDS_v260):
     pass
