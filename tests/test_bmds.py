c7ac8e4f27162e5365b7e2273fe6369a2a63e1d1
Andy Shapiro <shapiromatron@gmail.com>
2016-07-26 14:06:20 -0400

    update tests to use latest model versions

--- tests/test_bmds.py
+++ tests/test_bmds.py
@@ -7,11 +7,11 @@ class TestVersionsAndModels(unittest.TestCase):
 
     def test_get_versions(self):
         versions = sorted(bmds.get_versions())
-        expected = sorted(['2.40', '2.601', '2.31', '2.30', '2.60'])
+        expected = sorted(['BMDS230', 'BMDS231', 'BMDS240', 'BMDS260', 'BMDS2601'])
         self.assertListEqual(versions, expected)
 
     def test_get_models(self):
-        models = bmds.get_models_for_version('2.40')
+        models = bmds.get_models_for_version('BMDS240')
         dich_model_names = models['D'].keys()
         expected = [
             'Logistic', 'LogLogistic', 'Probit',
