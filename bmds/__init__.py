533e1ec402e51504fc1626378f3aa8e65089d755
Andy Shapiro <shapiromatron@gmail.com>
2016-10-20 17:15:11 -0400

    add monkeypatch utility to API

--- bmds/__init__.py
+++ bmds/__init__.py
@@ -4,3 +4,8 @@ from .session import * # noqa
 from .bmds import *  # noqa
 from .datasets import *  # noqa
 from models import *  # noqa
+
+try:
+    from .monkeypatch import *  # noqa
+except ImportError:
+    pass
