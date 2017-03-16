00c9d09cae3f181216d8f5dcc0dab40f06e73562
Andy Shapiro <shapiromatron@gmail.com>
2017-03-16 18:20:51 -0400

    fix test for settings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,6 +3,6 @@ from simple_settings.utils import settings_stub
 
 
 def test_settings():
-    assert settings.BMDS_MODEL_TIMEOUT_SECONDS == 5
+    assert settings.BMDS_MODEL_TIMEOUT_SECONDS == 10
     with settings_stub(BMDS_MODEL_TIMEOUT_SECONDS=1):
         assert settings.BMDS_MODEL_TIMEOUT_SECONDS == 1
