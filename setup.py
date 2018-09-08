2c9abbfd0e21144dea615c8499784ba3db40f0ad
Andy Shapiro <shapiromatron@gmail.com>
2018-09-08 10:25:25 -0400

    add flake8 config and whitelist python3.7

--- setup.py
+++ setup.py
@@ -55,5 +55,6 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
