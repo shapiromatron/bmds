c1969e449792488566b2bb67aa5ee0aa6dad5d41
Bishwo Bhandari <bhanbish@isu.edu>
2021-03-09 11:31:26 -0500

    -- added tables in report document.

--- bmds/bmds3/reporting.py
+++ bmds/bmds3/reporting.py
@@ -131,16 +131,245 @@ def write_models(report: Report, session, header_level: int):
             )
             continue
 
+        # Info Table
+        report.document.add_paragraph("Info Table", styles.tbl_body)
+        info_data = [
+            ["Model Name", model.name()],
+            ["Dataset Name", model.dataset.metadata.name],
+            ["Model Form", "To Do"],
+        ]
+        info_table = tabulate.tabulate(info_data, tablefmt="fancy_grid")
+        report.document.add_paragraph(info_table, styles.outfile)
+        report.document.add_paragraph()
+
+        if model.dataset.dtype is Dtype.CONTINUOUS:
+            # Model Options
+            report.document.add_paragraph("Model Options", styles.tbl_body)
+            options_data = [
+                ["BMR Type", model.settings.bmr_type],
+                ["BMRF", model.settings.bmr],
+                ["Tail Probability", model.settings.tail_prob],
+                ["Confidence Level", 1 - model.settings.alpha],
+                ["Distribution + Variance", model.settings.disttype],
+            ]
+            options_table = tabulate.tabulate(options_data, tablefmt="fancy_grid")
+            report.document.add_paragraph(options_table, styles.outfile)
+            report.document.add_paragraph()
+
+            # Model Data
+            report.document.add_paragraph("Model Data", styles.tbl_body)
+            model_data = [
+                ["Dependent Variable", "Dose"],
+                ["Independent Variable", "Mean"],
+                ["Number of Observations", len(model.dataset.doses)],
+                ["Adverse Direction", "Up"],
+            ]
+            model_table = tabulate.tabulate(model_data, tablefmt="fancy_grid")
+            report.document.add_paragraph(model_table, styles.outfile)
+            report.document.add_paragraph()
+
+        if model.dataset.dtype is Dtype.DICHOTOMOUS:
+            # Model Options
+            report.document.add_paragraph("Model Options", styles.tbl_body)
+            options_data = [
+                ["Risk Type", model.settings.bmr_type],
+                ["BMR", model.settings.bmr],
+                ["Confidence Level", 1 - model.settings.alpha],
+            ]
+            options_table = tabulate.tabulate(options_data, tablefmt="fancy_grid")
+            report.document.add_paragraph(options_table, styles.outfile)
+            report.document.add_paragraph()
+
+            # Model Data
+            report.document.add_paragraph("Info Table", styles.tbl_body)
+            model_data = [
+                ["Dependent Variable", "Dose"],
+                ["Independent Variable", "Fraction Affected"],
+                ["Number of Observations", len(model.dataset.doses)],
+                ["Adverse Direction", "Up"],
+            ]
+            model_table = tabulate.tabulate(model_data, tablefmt="fancy_grid")
+            report.document.add_paragraph(model_table, styles.outfile)
+            report.document.add_paragraph()
+
         # print figure
         add_mpl_figure(report.document, model.plot(), 6)
 
-        # demo - build params table
-        report.document.add_paragraph(
-            "This is just an example of what can be done...", styles.tbl_body
+        # continuous summary
+        if model.dataset.dtype is Dtype.CONTINUOUS:
+            report.document.add_paragraph("Continuous Summary", styles.tbl_body)
+            summary_headers = ["BMD", "BMDL", "BMDU", "AIC", "LL", "model_df", "chi-squared"]
+            summary_data = [
+                model.results.bmd,
+                model.results.bmdl,
+                model.results.bmdu,
+                model.results.fit.aic,
+                model.results.fit.loglikelihood,
+                model.results.fit.model_df,
+                model.results.fit.chisq,
+            ]
+            summary_table = tabulate.tabulate(
+                [summary_data], headers=summary_headers, tablefmt="fancy_grid"
+            )
+            report.document.add_paragraph(summary_table, styles.outfile)
+            report.document.add_paragraph()
+
+        # dichotomous summary
+        if model.dataset.dtype is Dtype.DICHOTOMOUS:
+            report.document.add_paragraph("Dichotomous Summary", styles.tbl_body)
+            summary_headers = [
+                "BMD",
+                "BMDL",
+                "BMDU",
+                "AIC",
+                "LL",
+                "model_df",
+                "p-value",
+                "DOF",
+                "chi-squared",
+            ]
+            summary_data = [
+                model.results.bmd,
+                model.results.bmdl,
+                model.results.bmdu,
+                model.results.fit.aic,
+                model.results.fit.loglikelihood,
+                model.results.fit.model_df,
+                model.results.gof.p_value,
+                model.results.fit.total_df,
+                model.results.fit.chisq,
+            ]
+            summary_table = tabulate.tabulate(
+                [summary_data], headers=summary_headers, tablefmt="fancy_grid"
+            )
+            report.document.add_paragraph(summary_table, styles.outfile)
+            report.document.add_paragraph()
+
+        # model parameters
+        report.document.add_paragraph("Model Parameters", styles.tbl_body)
+        param_headers = ["Variable", "Parameter", "Bounded"]
+        param_data = list(
+            map(
+                list,
+                zip(
+                    model.results.parameters.names,
+                    model.results.parameters.values,
+                    model.results.parameters.bounded,
+                ),
+            )
         )
-        tbl = tabulate.tabulate(
-            [model.results.parameters.values],
-            headers=model.bmd_model_class.params,
-            tablefmt="fancy_grid",
+        param_table = tabulate.tabulate(param_data, headers=param_headers, tablefmt="fancy_grid")
+        report.document.add_paragraph(param_table, styles.outfile)
+        report.document.add_paragraph()
+
+        # Goodness of Fit -Continuous
+        if model.dataset.dtype is Dtype.CONTINUOUS:
+            report.document.add_paragraph("Goodness of Fit", styles.tbl_body)
+            gof_headers = ["Dose", "Est. Prob", "Expected", "Observed", "Sized", "Scaled Res."]
+            gof_data = list(
+                map(
+                    list,
+                    zip(
+                        model.dataset.doses,
+                        model.results.gof.est_mean,
+                        model.results.gof.calc_mean,
+                        model.results.gof.obs_mean,
+                        model.results.gof.size,
+                        model.results.gof.residual,
+                    ),
+                )
+            )
+            gof_table = tabulate.tabulate(gof_data, headers=gof_headers, tablefmt="fancy_grid")
+            report.document.add_paragraph(gof_table, styles.outfile)
+            report.document.add_paragraph()
+
+            # deviance table
+            report.document.add_paragraph("Analysis of Deviance", styles.tbl_body)
+            deviance_headers = ["Model", "LL", "Num Params", "AIC"]
+            deviance_data = list(
+                map(
+                    list,
+                    zip(
+                        model.results.deviance.names,
+                        model.results.deviance.loglikelihoods,
+                        model.results.deviance.num_params,
+                        model.results.deviance.aics,
+                    ),
+                )
+            )
+            deviance_table = tabulate.tabulate(
+                deviance_data, headers=deviance_headers, tablefmt="fancy_grid"
+            )
+            report.document.add_paragraph(deviance_table, styles.outfile)
+            report.document.add_paragraph()
+
+            # Test of Interest
+            report.document.add_paragraph("Test of Interest", styles.tbl_body)
+            test_headers = ["Test", "Likelihood Ratio", "DF", "P Value"]
+            test_data = list(
+                map(
+                    list,
+                    zip(
+                        model.results.tests.names,
+                        model.results.tests.ll_ratios,
+                        model.results.tests.dfs,
+                        model.results.tests.p_values,
+                    ),
+                )
+            )
+            test_table = tabulate.tabulate(test_data, headers=test_headers, tablefmt="fancy_grid")
+            report.document.add_paragraph(test_table, styles.outfile)
+            report.document.add_paragraph()
+
+        # Goodness of Fit -Dichotomous
+        if model.dataset.dtype is Dtype.DICHOTOMOUS:
+            report.document.add_paragraph("Goodness of Fit", styles.tbl_body)
+            gof_headers = ["Dose", "Est. Prob", "Expected", "Observed", "Sized", "Scaled Res."]
+            gof_data = []
+            for dg in range(len(model.dataset.doses)):
+                gof_data.append(
+                    [
+                        model.dataset.doses[dg],
+                        model.results.gof.expected[dg] / model.dataset.ns[dg],
+                        model.results.gof.expected[dg],
+                        model.dataset.incidences[dg],
+                        model.dataset.ns[dg],
+                        model.results.gof.residual[dg],
+                    ]
+                )
+
+            gof_table = tabulate.tabulate(gof_data, headers=gof_headers, tablefmt="fancy_grid")
+            report.document.add_paragraph(gof_table, styles.outfile)
+            report.document.add_paragraph()
+
+            # deviance dichotomous
+            report.document.add_paragraph("Analysis of Deviance", styles.tbl_body)
+            deviance_headers = ["Model", "LL", "Num Params", "Deviance", "Test DF", "P Value"]
+            deviance_data = list(
+                map(
+                    list,
+                    zip(
+                        model.results.deviance.names,
+                        model.results.deviance.ll,
+                        model.results.deviance.params,
+                        model.results.deviance.deviance,
+                        model.results.deviance.df,
+                        model.results.deviance.p_value,
+                    ),
+                )
+            )
+            deviance_table = tabulate.tabulate(
+                deviance_data, headers=deviance_headers, tablefmt="fancy_grid"
+            )
+            report.document.add_paragraph(deviance_table, styles.outfile)
+            report.document.add_paragraph()
+
+        # CDF Table
+        report.document.add_paragraph("CDF Table", styles.tbl_body)
+        cdf_headers = ["Percentile", "BMD"]
+        cdf_list = list(
+            map(list, zip(model.results.fit.bmd_dist[1], model.results.fit.bmd_dist[0]))
         )
-        report.document.add_paragraph(tbl, styles.outfile)
+        cdf_table = tabulate.tabulate(cdf_list, headers=cdf_headers, tablefmt="fancy_grid")
+        report.document.add_paragraph(cdf_table, styles.outfile)
+        report.document.add_paragraph()
