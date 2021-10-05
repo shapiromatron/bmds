3625599a95d076d34f1f62883195400b730acdbc
Andy Shapiro <shapiromatron@gmail.com>
2021-10-05 17:13:23 -0400

    set to false?

--- bmds/bmds3/models/continuous.py
+++ bmds/bmds3/models/continuous.py
@@ -75,6 +75,7 @@ class BmdModelContinuous(BmdModel):
             ctypes.pointer(structs.aod),
             ctypes.pointer(structs.gof),
             ctypes.pointer(ctypes.c_bool(False)),
+            ctypes.pointer(ctypes.c_bool(False)),  # TODO - set False or dynamic?
         )
         self.results = ContinuousResult.from_model(self)
         return self.results
