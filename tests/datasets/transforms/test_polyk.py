5808d2183a1fed87d978b3835456a0bb6cf07e56
Andy Shapiro <shapiromatron@gmail.com>
2023-01-31 09:46:48 -0500

    survival-adjusted quantal response test (poly-k) (#98)
    * poly3 adjustment
    
    * add a calculate
    
    * revisions and docstrings
    
    * a few remaining docstrings
    
    * rename test file
    
    * remove default day adjustment
    
    * update test after changing back input
    
    * add one more test

---
+++ tests/datasets/transforms/test_polyk.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+
+from bmds.datasets.transforms import polyk
+
+
+def test_poly(data_path):
+    df = pd.read_csv(data_path / "datasets/transforms/polyk.csv")
+    assert df.shape == (200, 3)
+    assert df.columns.values.tolist() == ["dose", "day", "has_tumor"]
+
+    df2 = polyk.adjust_n(df)
+    assert df2.shape == (200, 4)
+    assert df2.columns.values.tolist() == ["dose", "day", "has_tumor", "adj_n"]
+
+    df3 = polyk.summary_stats(df2)
+    assert df3.shape == (4, 6)
+    assert df3.columns.values.tolist() == [
+        "dose",
+        "n",
+        "adj_n",
+        "incidence",
+        "proportion",
+        "adj_proportion",
+    ]
+    assert np.allclose(df3.adj_proportion, [0.1414, 0.2836, 0.5700, 0.6643], atol=1e-4)
+
+
+def test_calculate(data_path):
+    df = pd.read_csv(data_path / "datasets/transforms/polyk.csv")
+
+    # using no max day; derived from data
+    _, res = polyk.calculate(
+        doses=df.dose.tolist(),
+        day=df.day.tolist(),
+        has_tumor=df.has_tumor.tolist(),
+        k=3,
+    )
+    assert df.day.max() == 734
+    assert np.allclose(res.adj_proportion, [0.1414, 0.2836, 0.5700, 0.6643], atol=1e-4)
+
+    # fixing max_day to a 2 yr cancer bioassay duration
+    _, res = polyk.calculate(
+        doses=df.dose.tolist(),
+        day=df.day.tolist(),
+        has_tumor=df.has_tumor.tolist(),
+        k=3,
+        max_day=730,
+    )
+    assert np.allclose(res.adj_proportion, [0.1395, 0.2803, 0.5660, 0.6607], atol=1e-4)
