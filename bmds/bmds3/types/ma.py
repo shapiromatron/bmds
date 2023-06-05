2c90f1a8649b170fd671ec997fcabfbfb2bf85e7
hausman-gdit <karen.hausman@gdit.com>
2023-06-05 13:13:12 -0400

    rename MA things

--- bmds/bmds3/types/ma.py
+++ bmds/bmds3/types/ma.py
@@ -17,42 +17,42 @@ class ModelAverageResult(BaseModel):
 
 class DichotomousModelAverage:
     def __init__(self, dataset, models: list[BmdModelDichotomous], model_weights: npt.NDArray):
-        self.pyMA = bmdscore.python_dichotomousMA_analysis()
-        self.pyMA.nmodels = len(models)
-        self.pyMA.nparms = [model.structs.result.nparms for model in models]
-        self.pyMA.actual_parms = [
+        self.average = bmdscore.python_dichotomousMA_analysis()
+        self.average.nmodels = len(models)
+        self.average.nparms = [model.structs.result.nparms for model in models]
+        self.average.actual_parms = [
             model.structs.result.nparms for model in models
         ]  # TODO - different than prior code?
-        # self.pyMA.actual_parms = [model.structs.result.parms for model in models]
-        self.pyMA.prior_cols = [model.structs.analysis.prior_cols for model in models]
-        self.pyMA.models = [model.structs.analysis.model for model in models]
-        self.pyMA.priors = [model.structs.analysis.prior for model in models]
-        self.pyMA.modelPriors = model_weights
+        # self.average.actual_parms = [model.structs.result.parms for model in models]
+        self.average.prior_cols = [model.structs.analysis.prior_cols for model in models]
+        self.average.models = [model.structs.analysis.model for model in models]
+        self.average.priors = [model.structs.analysis.prior for model in models]
+        self.average.modelPriors = model_weights
 
         first = models[0].structs.analysis
-        self.pyAnal = bmdscore.python_dichotomous_analysis()
-        self.pyAnal.BMD_type = first.BMD_type
-        self.pyAnal.BMR = first.BMR
-        self.pyAnal.alpha = first.alpha
-        self.pyAnal.Y = dataset.incidences
-        self.pyAnal.n_group = dataset.ns
-        self.pyAnal.doses = dataset.doses
-        self.pyAnal.n = dataset.num_dose_groups
+        self.analysis = bmdscore.python_dichotomous_analysis()
+        self.analysis.BMD_type = first.BMD_type
+        self.analysis.BMR = first.BMR
+        self.analysis.alpha = first.alpha
+        self.analysis.Y = dataset.incidences
+        self.analysis.n_group = dataset.ns
+        self.analysis.doses = dataset.doses
+        self.analysis.n = dataset.num_dose_groups
 
-        self.pyMARes = bmdscore.python_dichotomousMA_result()
-        self.pyMARes.nmodels = len(models)
-        self.pyMARes.dist_numE = 200
-        self.pyMARes.models = [model.structs.result for model in models]
+        self.averageRes = bmdscore.python_dichotomousMA_result()
+        self.averageRes.nmodels = len(models)
+        self.averageRes.dist_numE = 200
+        self.averageRes.models = [model.structs.result for model in models]
 
         self.bmdsRes = bmdscore.BMDSMA_results()
-        self.bmdsRes.BMD = np.full(self.pyMA.nmodels, -9999)
-        self.bmdsRes.BMDL = np.full(self.pyMA.nmodels, -9999)
-        self.bmdsRes.BMDU = np.full(self.pyMA.nmodels, -9999)
-        self.bmdsRes.ebUpper = np.full(self.pyAnal.n, -9999)
-        self.bmdsRes.ebLower = np.full(self.pyAnal.n, -9999)
+        self.bmdsRes.BMD = np.full(self.average.nmodels, -9999)
+        self.bmdsRes.BMDL = np.full(self.average.nmodels, -9999)
+        self.bmdsRes.BMDU = np.full(self.average.nmodels, -9999)
+        self.bmdsRes.ebUpper = np.full(self.analysis.n, -9999)
+        self.bmdsRes.ebLower = np.full(self.analysis.n, -9999)
 
     def execute(self) -> "DichotomousModelAverageResult":
-        bmdscore.pythonBMDSDichoMA(self.pyMA, self.pyAnal, self.pyMARes, self.bmdsRes)
+        bmdscore.pythonBMDSDichoMA(self.average, self.analysis, self.averageRes, self.bmdsRes)
 
     def __str__(self):
         return dedent("TODO")
@@ -78,13 +78,13 @@ class DichotomousModelAverageResult(ModelAverageResult):
     @classmethod
     def from_cpp(cls, analysis: DichotomousModelAverage, model_results) -> Self:
         # only keep positive finite values
-        arr = np.array(analysis.pyMARes.bmd_dist).reshape(2, analysis.pyMARes.dist_numE).T
+        arr = np.array(analysis.averageRes.bmd_dist).reshape(2, analysis.averageRes.dist_numE).T
         arr = arr[np.isfinite(arr[:, 0])]
         arr = arr[arr[:, 0] > 0]
 
         # calculate dr_y for model averaging
-        priors = np.array(analysis.pyMA.modelPriors)
-        posteriors = np.array(analysis.pyMARes.post_probs)
+        priors = np.array(analysis.average.modelPriors)
+        posteriors = np.array(analysis.averageRes.post_probs)
         values = np.array([result.plotting.dr_y for result in model_results])
         dr_x = model_results[0].plotting.dr_x
         dr_y = values.T.dot(posteriors)
