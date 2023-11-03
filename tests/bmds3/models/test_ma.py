742c0883d9830358929269d8ac24c84ab63bcfe8
Andy Shapiro <shapiromatron@gmail.com>
2023-11-02 23:05:37 -0400

    remove bmds_model_average for base session

--- tests/bmds3/models/test_ma.py
+++ tests/bmds3/models/test_ma.py
@@ -17,7 +17,7 @@ class TestDichotomousMa:
         assert isinstance(json.dumps(d), str)
 
         # check bmd values exist and are valid
-        res = session.bmds_model_average.results
+        res = session.model_average.results
         assert np.allclose([57.1, 65.9, 75.0], [res.bmdl, res.bmd, res.bmdu], atol=5)
 
     def test_prior_weights(self, ddataset2):
@@ -28,8 +28,8 @@ class TestDichotomousMa:
         session.add_model_averaging()
         assert np.allclose(session.ma_weights, [0.5, 0.5])
         session.execute()
-        assert np.allclose(session.bmds_model_average.results.priors, [0.5, 0.5])
-        assert np.allclose(session.bmds_model_average.results.posteriors, [0.11, 0.89], atol=0.05)
+        assert np.allclose(session.model_average.results.priors, [0.5, 0.5])
+        assert np.allclose(session.model_average.results.posteriors, [0.11, 0.89], atol=0.05)
 
         # custom; propagate through results
         session = bmds.session.Bmds330(dataset=ddataset2)
@@ -38,8 +38,8 @@ class TestDichotomousMa:
         session.add_model_averaging(weights=[0.9, 0.1])
         assert np.allclose(session.ma_weights, [0.9, 0.1])
         session.execute()
-        assert np.allclose(session.bmds_model_average.results.priors, [0.9, 0.1])
-        assert np.allclose(session.bmds_model_average.results.posteriors, [0.53, 0.47], atol=0.05)
+        assert np.allclose(session.model_average.results.priors, [0.9, 0.1])
+        assert np.allclose(session.model_average.results.posteriors, [0.53, 0.47], atol=0.05)
 
     @pytest.mark.mpl_image_compare
     def test_bmds3_dichotomous_ma_plot(self, ddataset2):
@@ -48,4 +48,4 @@ class TestDichotomousMa:
         session.add_model(bmds.constants.M_Probit, {"priors": PriorClass.bayesian})
         session.add_model_averaging(weights=[0.9, 0.1])
         session.execute()
-        return session.bmds_model_average.plot()
+        return session.model_average.plot()
