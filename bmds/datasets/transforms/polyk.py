cf7b1b65dac43fd99e25191b8a067028d6c589aa
Andy Shapiro <shapiromatron@gmail.com>
2023-12-10 19:09:18 -0500

    Add Poly K word and Excel Reports (#128)
    * add initial report
    
    * write excel report too
    
    * respect the rewrite flag
    
    * basic tables and doc format
    
    * updates w/ bmds online integration
    
    * updates from review
    
    * use optimized quick printing docx table function
    
    * add dose units to report figures
    
    ---------
    
    Co-authored-by: hausman-gdit <karen.hausman@gdit.com>

--- bmds/datasets/transforms/polyk.py
+++ bmds/datasets/transforms/polyk.py
@@ -6,87 +6,208 @@ for carcinogenicity in small samples. Biometrics. 1988 Jun;44(2):417-31.
 PMID: 3390507. DOI: 10.2307/2531856
 """
 
+from io import BytesIO
+from itertools import cycle
+
+import matplotlib.ticker as mtick
 import pandas as pd
+from matplotlib.figure import Figure
+
+from ... import plotting
+from ...bmds3.reporting import df_to_table, write_setting_p
+from ...reporting.styling import Report, add_mpl_figure
 
 
-def adjust_n(df: pd.DataFrame, k: float | None = 3, max_day: int | None = None) -> pd.DataFrame:
-    """Adjust the n for individual observations in a dataset.
+class PolyKAdjustment:
+    def __init__(
+        self,
+        doses: list[float],
+        day: list[int],
+        has_tumor: list[int],
+        k: float = 3,
+        max_day: float | None = None,
+        dose_units: str = "",
+        dose_name: str = "Dose",
+        time_units: str = "days",
+    ) -> None:
+        """Adjust tumor data via poly K adjustment.
 
-    Args:
-        df (pd.DataFrame): a DataFrame of dataset used to manipulate. Three columns:
-            - dose (float >=0)
-            - day (integer >=0)
-            - has_tumor (integer, 0 or 1)
-        k (Optional[float], optional, default 3): The adjustment term to apply
-        max_day (Optional[int], optional): The maximum data. If specific, the value is used,
-            otherwise, it is calculated from the maximum reported day in the dataset
+        Args:
+            doses (list[float]): a list of floats
+            day (list[int]): a list of days or other time measurements
+            has_tumor (list[int]): integer, 0 or 1, where 1 is has_tumor is True
+            k (float | None, optional): The adjustment term to apply. Defaults to 3.
+            max_day (float | None, optional): The maximum data. If specific, the value is used,
+                otherwise, it is calculated from the maximum reported day in the dataset.
+            dose_units (str): Dose Units, for graphing
+            dose_name (str): the term for the dose axis. Defaults to "Dose".
+            time_units (str): the time units, defaults to "days".
+        """
+        self.k = k
+        self.max_day = max_day
+        self.dose_units = dose_units
+        self.dose_name = dose_name
+        self.time_units = time_units
+        self.input_data = pd.DataFrame(dict(dose=doses, day=day, has_tumor=has_tumor))
+        self.adjusted_data = self.calc_adjusted_n()
+        self.summary = self.calc_summary_stats()
 
-    Returns:
-        pd.DataFrame: A copy of the original dataframe, with an new column `adj_n`
-    """
-    columns = ["dose", "day", "has_tumor"]
-    if df.columns.tolist() != columns:
-        raise ValueError(f"Unexpected column names; expecting {columns}")
-    if set(df.has_tumor.unique()) != {0, 1}:
-        raise ValueError("Expected `has_tumor` values must be 0 and 1")
-    df = df.copy()
-    if max_day is None:
-        max_day = df.day.max()
-    df.loc[:, "adj_n"] = (df.query("has_tumor==0").day / max_day) ** k
-    df.loc[:, "adj_n"] = df.loc[:, "adj_n"].fillna(1).clip(upper=1)
-    return df
+    def calc_adjusted_n(self) -> pd.DataFrame:
+        """Adjust N for individual observations in a dataset."""
+        columns = ["dose", "day", "has_tumor"]
+        if self.input_data.columns.tolist() != columns:
+            raise ValueError(f"Unexpected column names; expecting {columns}")
+        if set(self.input_data.has_tumor.unique()) != {0, 1}:
+            raise ValueError("Expected `has_tumor` values must be 0 and 1")
+        df = self.input_data.copy()
+        max_day = self.max_day or df.day.max()
+        df.loc[:, "adj_n"] = (df.query("has_tumor==0").day / max_day) ** self.k
+        df.loc[:, "adj_n"] = df.loc[:, "adj_n"].fillna(1).clip(upper=1)
+        return df
 
+    def calc_summary_stats(self) -> pd.DataFrame:
+        """Calculate grouped summary statistics by group for Poly K adjusted data."""
+        columns = ["dose", "day", "has_tumor", "adj_n"]
+        if self.adjusted_data.columns.tolist() != columns:
+            raise ValueError(f"Unexpected column names: {columns}")
+        grouped = self.adjusted_data.groupby("dose")
+        df2 = pd.DataFrame(
+            data=[
+                grouped.has_tumor.count().rename("n", inplace=True),
+                grouped.adj_n.sum().rename("adj_n", inplace=True),
+                grouped.has_tumor.sum().rename("incidence", inplace=True),
+            ]
+        ).T.reset_index()
+        df2.loc[:, "proportion"] = df2.incidence / df2.n
+        df2.loc[:, "adj_proportion"] = df2.incidence / df2.adj_n
+        return df2
 
-def summary_stats(df: pd.DataFrame) -> pd.DataFrame:
-    """Calculate summary statistics by group for poly k adjusted data, which can be used for
-    dichotomous dose-response modeling.
+    def summary_figure(self, figsize: tuple[float, float] | None = None) -> Figure:
+        fig = plotting.create_empty_figure(figsize=figsize)
+        ax = fig.gca()
 
-    Args:
-        df (pd.DataFrame): The input dataframe of individual response data with adjustment
-            calculated from the `adjust_n` method above.
+        ax.set_title("Adjusted Proportion vs Original Proportion")
+        ax.set_xlabel(
+            f"{self.dose_name} ({self.dose_units})" if self.dose_units else self.dose_name
+        )
+        ax.set_ylabel("Proportion (%)")
+        ax.margins(plotting.PLOT_MARGINS)
 
-    Returns:
-        pd.DataFrame: A dataframe of group-level values, both adjusted and unadjusted
-    """
-    columns = ["dose", "day", "has_tumor", "adj_n"]
-    if df.columns.tolist() != columns:
-        raise ValueError(f"Unexpected column names: {columns}")
-    grouped = df.groupby("dose")
-    df2 = pd.DataFrame(
-        data=[
-            grouped.has_tumor.count().rename("n", inplace=True),
-            grouped.adj_n.sum().rename("adj_n", inplace=True),
-            grouped.has_tumor.sum().rename("incidence", inplace=True),
-        ]
-    ).T.reset_index()
-    df2.loc[:, "proportion"] = df2.incidence / df2.n
-    df2.loc[:, "adj_proportion"] = df2.incidence / df2.adj_n
-    return df2
+        ax.plot(
+            self.summary.dose,
+            self.summary.proportion,
+            "o-",
+            color="blue",
+            label="Original Proportion",
+            markersize=8,
+            markeredgewidth=1,
+            markeredgecolor="white",
+        )
+        ax.plot(
+            self.summary.dose,
+            self.summary.adj_proportion,
+            "^-",
+            color="red",
+            label="Adjusted Proportion",
+            markersize=8,
+            markeredgewidth=1,
+            markeredgecolor="white",
+        )
 
+        legend = ax.legend(**plotting.LEGEND_OPTS)
+        for handle in legend.legend_handles:
+            handle.set_markersize(8)
 
-def calculate(
-    doses: list[float],
-    day: list[int],
-    has_tumor: list[int],
-    k: float | None = 3,
-    max_day: int | None = None,
-) -> tuple[pd.DataFrame, pd.DataFrame]:
-    """Calculate polyk adjustment on a dataset
+        ax.yaxis.set_major_formatter(mtick.PercentFormatter(1))
+        return fig
 
-    Args:
-        doses (list[float]): A list of dose values
-        day (list[int]): A list of days when observed
-        has_tumor (list[int]): Binary flag for if entity had a tumor
-        k (Optional[float], optional): Poly k adjustment value; defaults to 3.
-        max_day (Optional[int], optional): Maximum observation day; defaults to calculating from
-            dataset,
+    def tumor_incidence_figure(self, figsize: tuple[float, float] | None = None) -> Figure:
+        markers = "o^svDP"
+        df = self.input_data.copy()
+        df.loc[:, "cummulative_tumor"] = df.groupby("dose").has_tumor.cumsum()
 
-    Returns:
-        tuple[pd.DataFrame, pd.DataFrame]: Two data frames. The first is the individual data
-            with the adjusted value, the second is summary data showing adjusted and unadjusted
-            incidence data for use in dichotomous dose response modeling.
-    """
-    df = pd.DataFrame(dict(dose=doses, day=day, has_tumor=has_tumor))
-    df2 = adjust_n(df, k, max_day)
-    df3 = summary_stats(df2)
-    return df2, df3
+        marker = cycle(markers)
+
+        fig = plotting.create_empty_figure(figsize=figsize)
+        ax = fig.gca()
+        ax.set_xlabel(f"Study duration ({self.time_units})")
+        ax.set_ylabel("Cumulative tumor incidence")
+        ax.margins(plotting.PLOT_MARGINS)
+        ax.set_title("Tumor incidence over study duration")
+
+        dose_units = f" {self.dose_units}" if self.dose_units else ""
+
+        for value, d in df.groupby("dose"):
+            ax.plot(
+                d.day,
+                d.cummulative_tumor,
+                f"{next(marker)}-",
+                label=str(value) + dose_units,
+                markersize=8,
+                markeredgewidth=1,
+                markeredgecolor="white",
+            )
+
+        legend = ax.legend(**plotting.LEGEND_OPTS)
+        for handle in legend.legend_handles:
+            handle.set_markersize(8)
+
+        return fig
+
+    def write_docx_adjustment_table(self, report: Report):
+        """Add adjusted input data table to the document."""
+        df_to_table(report, self.adjusted_data)
+
+    def write_docx_summary_table(self, report: Report):
+        """Add a 'result'' data table with adjusted figures to the document."""
+        df_to_table(report, self.summary)
+
+    def to_docx(
+        self,
+        report: Report | None = None,
+        header_level: int = 1,
+        show_title: bool = True,
+    ):
+        """Returns a word document report of the Poly K Adjustment Calculation.
+
+        Args:
+            report (Report | None, optional): A optional report instance, otherwise create one.
+            header_level (int, optional): The top-level header level, defaults to 1.
+            show_title (bool, optional): Show the top level title, defaults True.
+        """
+        if report is None:
+            report = Report.build_default()
+
+        h1 = report.styles.get_header_style(header_level)
+        h2 = report.styles.get_header_style(header_level + 1)
+
+        if show_title:
+            report.document.add_paragraph("Poly K Adjustment", h1)
+
+        report.document.add_paragraph("Summary", h2)
+        write_setting_p(report, "K Adjustment Factor: ", str(self.k))
+        if self.max_day:
+            write_setting_p(report, "Maximum Day: ", str(self.max_day))
+
+        report.document.add_paragraph(self.write_docx_summary_table(report))
+        report.document.add_paragraph(add_mpl_figure(report.document, self.summary_figure(), 6))
+
+        report.document.add_paragraph("Individual Adjustments", h2)
+        report.document.add_paragraph(
+            add_mpl_figure(report.document, self.tumor_incidence_figure(), 6)
+        )
+        self.write_docx_adjustment_table(report)
+
+        return report.document
+
+    def to_excel(self) -> BytesIO:
+        """Returns an Excel report with worksheets summarizing the adjustment.
+
+        Returns:
+            BytesIO: An Excel worksheets.
+        """
+        f = BytesIO()
+        with pd.ExcelWriter(f) as writer:
+            for name, df in [("adjusted", self.adjusted_data), ("summary", self.summary)]:
+                df.to_excel(writer, sheet_name=name, index=False)
+        return f
